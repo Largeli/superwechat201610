466798908d06befe99aed797ad8c6fa4451424c6
lijianjun <1020942021@qq.com>
2017-02-15 19:00:38 +0800

    隐藏消息列表顶部的TitleBar

--- superwechat/src/cn/ucai/superwechat/ui/ConversationListFragment.java
+++ superwechat/src/cn/ucai/superwechat/ui/ConversationListFragment.java
@@ -17,13 +17,14 @@ import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMConversation;
 import com.hyphenate.chat.EMConversation.EMConversationType;
 import com.hyphenate.chat.EMMessage;
-import cn.ucai.superwechat.Constant;
-import cn.ucai.superwechat.R;
-import cn.ucai.superwechat.db.InviteMessgeDao;
 import com.hyphenate.easeui.model.EaseAtMessageHelper;
 import com.hyphenate.easeui.ui.EaseConversationListFragment;
 import com.hyphenate.easeui.widget.EaseConversationList.EaseConversationListHelper;
 import com.hyphenate.util.NetUtils;
+
+import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.db.InviteMessgeDao;
 
 public class ConversationListFragment extends EaseConversationListFragment{
 
@@ -35,6 +36,7 @@ public class ConversationListFragment extends EaseConversationListFragment{
         View errorView = (LinearLayout) View.inflate(getActivity(),R.layout.em_chat_neterror_item, null);
         errorItemContainer.addView(errorView);
         errorText = (TextView) errorView.findViewById(R.id.tv_connect_errormsg);
+        hideTitleBar();
     }
     
     @Override
